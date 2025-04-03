f318229ea440cdb795d9cc9e17e92d1471246a51
ishakorangi <korangiisha91@gmail.com>
2025-04-03 08:21:16 +0000

    hi

---
+++ exercise_list3.py
@@ -0,0 +1,17 @@
+marks = []
+
+
+while True:
+     user_mark = ("Enter a mark, or press enter to stop")
+     if user_mark == "":
+          break
+     else:
+          marks.append(user_mark)
+
+
+print("your total ma
+for mark in marks:
+     print(mark)
+mark_length = len(marks) 
+print(mark_length)
+print_
